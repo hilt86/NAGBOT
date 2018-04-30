597a5f760b262b90abfb693b92d6a45f3b9e2818
Hilton De Meillon <hdemeillon@telenet.com.au>
2018-04-30 02:20:59 +0000

    added example output of random integer - commit author fixed

--- nagbot/nagbot/nagbot.py
+++ nagbot/nagbot/nagbot.py
@@ -1,6 +1,7 @@
 # all the imports
 import os
 import sqlite3
+import random
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
     
@@ -17,3 +18,21 @@ app.config.update(dict(
 # setting silent to False will make the app complain if the environment
 # variable is not set
 app.config.from_envvar('NAGBOT_SETTINGS', silent=True)
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+        
+@app.route("/")
+def hello():
+    return  "Hello team!" + " here is a random integer : " + str(random.randint(1,101))
