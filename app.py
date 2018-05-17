64a516875ee454bf921bb40f45dba14e0eca422d
Hilton De Meillon <hdemeillon@telenet.com.au>
2018-05-17 15:51:25 +1000

    updated config import

--- app.py
+++ app.py
@@ -156,5 +156,5 @@ def api_json_nagbot():
         
 
 if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
+        #port = int(os.environ.get("PORT", 5000))
         app.run(host='0.0.0.0')
