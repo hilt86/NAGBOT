ef0d9bfe0c6904155975f123e2d04e67496f8f90
John Omer-Cooper <u1078532@umail.usq.edu.au>
2018-05-18 08:28:10 +1000

    Removed Slack connection code

--- qanda.py
+++ qanda.py
@@ -21,35 +21,11 @@ def qanda(user_id, ip_add, slack_client, slack_channel, nagbot_user_id, admin, r
     name="<@"+user_id+">"
     admin_user="<@"+admin+">"
     question="At " + timestamp + " Have you just logged in from "+ip_add+" ? yes or no "
-    if slack_client.rtm_connect():
-        logger.debug('Connected to Slack ...')
-        # print("NagBot connected and running!")
-        response = "Hi," + name + "\n"+ question
-        slack_client.api_call("chat.postMessage", channel=slack_channel, text=response, as_user=True)
-        while True & resp_time >= 0:
-            if resp_time == 0:
-                time_out(name, slack_client, ip_add)
-                return
-            new_evts = slack_client.rtm_read()
-            for evt in new_evts:
-                if "type" in evt:
-                    if evt['type']=="message" and evt['channel']==slack_channel:
-                        if evt['user'] == user_id and evt['user'] != admin and "<@" + nagbot_user_id + ">" not in evt['text']:
-                            user_info=slack_client.api_call("users.info", user=evt['user'])
-                            answer=evt['text']
-                            response = response_option(answer, name, admin, admin_user, slack_client, slack_channel, nagbot_user_id,resp_time,ip_add)
-                            slack_client.api_call("chat.postMessage", channel=evt['channel'], text=response, as_user=True)
-                            return
-            time.sleep(1)
-            resp_time -=1
-    else:
-        # print "Connection Failed, invalid token?"
-        logger.warning("Connection Failed, invalid token?")
+   
         
 def askQuestion(name, question, slack_client, slack_channel, nagbot_user_id, admin, resp_time):
     if slack_client.rtm_connect():
         response = name + question
-        slack_client.api_call("chat.postMessage", channel=slack_channel, text=response, as_user=True)
     else:
         # print "Connection Failed, invalid token?"
         logger.warning("Connection Failed, invalid token?")
@@ -64,36 +40,7 @@ def grabResponses(name, slack_client, slack_channel, nagbot_user_id ):
     else:
         logger.warning("Connection Failed, invalid token?")
         # print "Connection Failed, invalid token?"
-"""
- ? don't need this unless we want to dynamically retrieve user names from Slack.
- 
-def listUsers(name, question, slack_client, slack_channel, nagbot_user_id, admin, resp_time):
-    if slack_client.rtm_connect():
-        channel_list = requests.get('https://slack.com/api/channels.list?token=%s' % SLACK_API_TOKEN).json()['channels']
-        channel = filter(lambda c: c['name'] == CHANNEL_NAME, channel_list)[0]
-        channel_info = requests.get('https://slack.com/api/channels.info?token=%s&channel=%s' % (SLACK_API_TOKEN, channel['id'])).json()['channel']
-        members = channel_info['members']
-        users_list = requests.get('https://slack.com/api/users.list?token=%s' % SLACK_API_TOKEN).json()['members']
-        users = filter(lambda u: u['id'] in members, users_list)
-    
-    for user in users:
-        print(user['real_name'])
-        print(user['id'])
-    else:
-        print "Connection Failed, invalid token?"
-    return
-"""
 
-def response_option(answer, name, admin, admin_user, slack_client, slack_channel, nagbot_user_id,resp_time, ip_add):
-    # This function provides the actions based on user answers.
-    if answer.lower() == "no":
-        escalate(name, slack_client, ip_add)
-    elif answer.lower() == "yes":
-        reply = "Great carry on !"
-        return reply
-    else:
-        question = "Not sure what you mean. Please answer yes or no."
-        qanda(name, question, slack_client, slack_channel, nagbot_user_id, admin, resp_time)
 
 def escalate(name, slack_client, ip_add):
     logger.warning("Escalation Detected !")
