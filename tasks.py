03facfceaaaa8f5d8eca370bab6957738f971327
Hilton De Meillon <hdemeillon@telenet.com.au>
2018-05-22 18:20:00 +1000

    added app context

--- tasks.py
+++ tasks.py
@@ -4,5 +4,5 @@ app = celery.Celery('example')
 @app.task
 def my_background_task(arg1, arg2):
     # some long running task here
-    print(" ### I am a background task ### ")
-    return True
+    with app.app_context():
+        print(" ### I am a background task ### ")
