3f5640ddc7b7d9359c9f83ecbd13830326d877e6
Bandr Talie O Alkhuzaie <u1078238@umail.usq.edu.au>
2018-05-22 14:06:23 +1000

    Update createrule.py

--- createrule.py
+++ createrule.py
@@ -1,13 +1,12 @@
 import yaml
 
 def createrules(es_host, es_port, nb_url):
-    # 1: take the es_host, es_port and nb_url variables and insert the values into the jinja2 template file (test file is used for now) using YAML formate 
     data = dict(es_host = es_host, es_port = es_port, nb_url = nb_url)
     with open(filepath, 'a') as outfile:
         yaml.dump(data, outfile, default_flow_style=False)
         outfile.close()
-    
-    # Return values
+        
+
 def yaml_loader(filepath):
    with open(filepath, "r") as file_descriptor:
        data = yaml.load(file_descriptor)
@@ -25,4 +24,3 @@ if __name__ == '__main__':
     data = yaml_loader(filepath)
     for n, m in data.iteritems():
         print n, ":", m
-        
